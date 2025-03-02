e7e41553822f39be579bf1716c615e487dd2034c
aam-bd <abdullah.al.mamun12@g.bracu.ac.bd>
2025-03-02 23:56:12 +0600

    Updated

---
+++ script/complete.js
@@ -0,0 +1,89 @@
+const complete_btn = document.querySelector("#fix-mobile-btn");
+complete_btn.addEventListener("click", function(){
+const task = document.querySelector("#task-to-complete");
+const value_task1 = task.innerText;
+const new_value = Number(value_task1) - 1;
+task.innerText = new_value;
+const task_completed = document.querySelector("#task-completed");
+const value_t = task_completed.innerText;
+const new_v = Number(value_t) + 1;
+task_completed.innerText = new_v;
+alert("Board Updated Successfully");
+complete_btn.setAttribute("disabled", "true")
+});
+
+const complete_btn1 = document.querySelector("#dark-mode");
+complete_btn1.addEventListener("click", function(){
+const task = document.querySelector("#task-to-complete");
+const value_task1 = task.innerText;
+const new_value = Number(value_task1) - 1;
+task.innerText = new_value;
+const task_completed = document.querySelector("#task-completed");
+const value_t = task_completed.innerText;
+const new_v = Number(value_t) + 1;
+task_completed.innerText = new_v;
+alert("Board Updated Successfully");
+complete_btn.setAttribute("disabled", "true")
+ });
+
+const complete_btn2 = document.querySelector("#openai");
+complete_btn2.addEventListener("click", function(){
+const task = document.querySelector("#task-to-complete");
+const value_task1 = task.innerText;
+const new_value = Number(value_task1) - 1;
+task.innerText = new_value;
+const task_completed = document.querySelector("#task-completed");
+const value_t = task_completed.innerText;
+const new_v = Number(value_t) + 1;
+task_completed.innerText = new_v;
+alert("Board Updated Successfully");
+complete_btn.setAttribute("disabled", "true")
+ });
+
+const complete_btn3 = document.querySelector("#home-page");
+complete_btn3.addEventListener("click", function(){
+const task = document.querySelector("#task-to-complete");
+const value_task1 = task.innerText;
+const new_value = Number(value_task1) - 1;
+task.innerText = new_value;
+const task_completed = document.querySelector("#task-completed");
+const value_t = task_completed.innerText;
+const new_v = Number(value_t) + 1;
+task_completed.innerText = new_v;
+alert("Board Updated Successfully");
+complete_btn.setAttribute("disabled", "true")
+ });
+
+const complete_btn4 = document.querySelector("#emoji");
+complete_btn4.addEventListener("click", function(){
+const task = document.querySelector("#task-to-complete");
+const value_task1 = task.innerText;
+const new_value = Number(value_task1) - 1;
+task.innerText = new_value;
+const task_completed = document.querySelector("#task-completed");
+const value_t = task_completed.innerText;
+const new_v = Number(value_t) + 1;
+task_completed.innerText = new_v;
+alert("Board Updated Successfully");
+complete_btn.setAttribute("disabled", "true")
+ });
+
+const complete_btn5 = document.querySelector("#job-search");
+complete_btn5.addEventListener("click", function(){
+const task = document.querySelector("#task-to-complete");
+const value_task1 = task.innerText;
+const new_value = Number(value_task1) - 1;
+task.innerText = new_value;
+const task_completed = document.querySelector("#task-completed");
+const value_t = task_completed.innerText;
+const new_v = Number(value_t) + 1;
+task_completed.innerText = new_v;
+alert("Board Updated Successfully");
+complete_btn.setAttribute("disabled", "true")
+ })
+
+
+
+
+
+
